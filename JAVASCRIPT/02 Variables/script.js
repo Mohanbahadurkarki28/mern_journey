42ae1cd48c6ced09fb36ac47056e18aa749479c7
mohankarki28 <mohankarki86555@gmail.com>
2024-09-20 17:49:31 +0545

    second

---
+++ JAVASCRIPT/02 Variables/script.js
@@ -0,0 +1,15 @@
+var firstName = 'Mohan'
+let lastName = 'Karki'
+let age = 15
+let खुशहै = true
+firstName = 100
+var a
+
+const hoursInDay = 24
+
+console.log(firstName)
+
+let userIntro = 
+    'Hi, my name is ' 
+    + firstName + ' ' 
+    + lastName + '. I am ' + age + ' ' + 'years old.'
---
+++ JAVASCRIPT/03 code-execution in JS/script.js
@@ -0,0 +1,6 @@
+var firstName = 'Mohan'
+let lastName = 'Karki'
+let age = 22 
+const yearOfBirth = 2002
+
+let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
