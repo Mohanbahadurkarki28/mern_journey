3a000deaaff32a0451fd590df61934fd628b69fe
mohankarki28 <mohankarki86555@gmail.com>
2024-09-23 16:26:46 +0545

    third

---
+++ JAVASCRIPT/09 Decision Making/script.js
@@ -0,0 +1,12 @@
+const username ='Mohan';
+const userAge = 28;
+
+console.log('Name: ${username}');
+console.log('Age: ${userAge}');
+
+if(userAge >=18 && userAge <= 24) 
+{
+    console.log('User is a College Student.'); 
+    console.log('And he is learning Programming .');
+}
+console.log('Program Ended');
