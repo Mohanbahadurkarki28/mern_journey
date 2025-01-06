dafe50006ee9ff033b18470e8cc9f9ae16b81996
mohankarki28 <mohankarki86555@gmail.com>
2025-01-06 09:28:20 +0545

    second

---
+++ JAVASCRIPT/27 Decision Making in JavaScript Using IF Statement/script.js
@@ -0,0 +1,33 @@
+const username = prompt('Plese enter your name.') || 'Mohan';
+const userage = prompt('Please enter your age.') || 22;
+console.log(`Name: ${username}`);
+console.log(`Age:${userage}`)
+
+if(0){
+    console.log("HELLO")
+}else{
+    console.log("WORLD")
+}
+
+/* if(userage>=0 && userage<=4){
+    console.log(`${username} is a kid.`)
+    console.log('and he is playing')
+}else if(userage>=5 && userage<=17){
+    console.log(`${username} is a school student.`)
+    console.log('and he is learning computer science')
+}else if(userage>=18 && userage<=24){
+    console.log(`${username} is a college student.`)
+    console.log('and he is learning computer science')
+}else if(userage>=25 && userage<=45){
+    console.log(`${username} is a Working professional.`)
+    console.log('and he is enjoying very well')
+}else if(userage>=45 && userage<=120){
+    console.log(`${username} is Retired.`)
+    console.log('He is reading the newspaper')
+}else if(userage>=121){
+    console.log(`${username} is immortal`)
+}else{
+    console.log('Please enter a valid age')
+}
+ */
+console.log('Program Ended!!');
