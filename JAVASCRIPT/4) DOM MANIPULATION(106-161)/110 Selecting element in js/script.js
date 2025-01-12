410877c3afae76baea95ff4da38ebf28dac51eef
Mohan Karki <Mohankarki86555@gmail.com>
2025-01-12 10:02:32 +0545

    '1'

--- JAVASCRIPT/4) DOM MANIPULATION(106-161)/110 Selecting element in js/script.js
+++ JAVASCRIPT/4) DOM MANIPULATION(106-161)/110 Selecting element in js/script.js
@@ -1,4 +1,14 @@
-function sayHi() {
+/* function sayHi() {
   document.body.children[4].src =
     'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg'
 }
+ */
+
+/* const allImages = document.getElementsByTagName('img')
+// const allImages1 = document.images
+// const cssImage = document.getElementsByClassName('css-images')[0]
+const cssImage = document.getElementById('css-image') */
+
+const cssImage = document.querySelectorAll('.css-image')
+
+
